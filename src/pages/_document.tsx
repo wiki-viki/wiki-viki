d0444867e108a1286e0ad0064febc0e7236d9b3d
easyhyun00 <leejihyun0324@gmail.com>
2024-06-20 15:12:51 +0900

    📦️ Chore: 폴더 구조 설정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,8 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
+const Document = () => {
   return (
-    <Html lang="en">
+    <Html lang="ko">
       <Head />
       <body>
         <Main />
@@ -10,4 +10,6 @@ export default function Document() {
       </body>
     </Html>
   );
-}
+};
+
+export default Document;
