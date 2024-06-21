1ee32a0b958ae99273723463b7dac40848200f91
easyhyun00 <leejihyun0324@gmail.com>
2024-06-21 15:16:48 +0900

    ✨ Feat: useBoolean 훅 구현 및 적용

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,24 +1,13 @@
-import React, { useState } from 'react';
 import Modal from '@/components/common/Modal';
+import useBoolean from '@/hooks/useBoolean';
 
 const Test = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const { value, handleOn, handleOff } = useBoolean();
 
   return (
     <div>
-      <button
-        onClick={() => {
-          return setIsOpen(!isOpen);
-        }}
-      >
-        버튼
-      </button>
-      <Modal
-        isOpen={isOpen}
-        onClose={() => {
-          return setIsOpen(!isOpen);
-        }}
-      >
+      <button onClick={handleOn}>버튼</button>
+      <Modal isOpen={value} onClose={handleOff}>
         <div>
           하이
           <p>안녕~~~~~~~</p>
