b0a700dfed3d3d27692010bf4abd764d5b0fd49f
easyhyun00 <leejihyun0324@gmail.com>
2024-06-21 16:43:49 +0900

    🔨 Refactor: 모달 div 구조 변경

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -23,18 +23,19 @@ const Modal = ({ children, isOpen, onClose }: ModalProps) => {
   return createPortal(
     <AnimatePresence>
       {isOpen && (
-        <motion.aside initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
-          <div
-            ref={modalRef}
-            onClick={handleClickOutside}
-            className="fixed inset-0 flex items-center justify-center bg-grayscale-500/30"
-          >
-            <div className="mx-2 flex h-auto w-[335px] flex-col rounded-[10px] bg-white p-[20px] shadow-lg md:w-[395px]">
-              <div className="flex justify-end">
-                <CloseIcon className="cursor-pointer" onClick={onClose} />
-              </div>
-              {children}
+        <motion.aside
+          className="fixed inset-0 flex items-center justify-center bg-grayscale-500/30"
+          ref={modalRef}
+          onClick={handleClickOutside}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <div className="mx-2 flex h-auto w-[335px] flex-col rounded-[10px] bg-white p-[20px] shadow-lg md:w-[395px]">
+            <div className="flex justify-end">
+              <CloseIcon className="cursor-pointer" onClick={onClose} />
             </div>
+            {children}
           </div>
         </motion.aside>
       )}
