6eaa00f4e9f2723930487bbfa1623fcc5397a485
Lin <shl6534@naver.com>
2024-06-21 18:59:14 +0900

    💄 Design: margin-bottom 속성 추가

--- src/components/Auth/AuthContainer.tsx
+++ src/components/Auth/AuthContainer.tsx
@@ -2,7 +2,7 @@ import { AuthContainerProps } from '@/types/AuthType';
 
 const AuthContainer = ({ title, children }: AuthContainerProps) => (
   <main className="container md:w-[335px] lg:w-[400px]">
-    <span className="text-2xl-semibold text-grayscale-500 text-center block">{title}</span>
+    <span className="text-2xl-semibold text-grayscale-500 text-center mb-[50px] block">{title}</span>
     {children}
   </main>
 );
