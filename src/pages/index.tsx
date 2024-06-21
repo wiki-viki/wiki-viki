b4858e3bf0460d2181d23a76530803a12ae665d9
dudwns0213 <dudwns0213@gmail.com>
2024-06-22 00:09:25 +0900

    ✅ Test: git action test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ const Home = () => {
     <main>
       <p className="text-lg text-main500">프로젝트 초기 세팅</p>
       <p className="text-main500">vercel 배포</p>
+      <p className="text-main500">배포 테스트1</p>
     </main>
   );
 };
