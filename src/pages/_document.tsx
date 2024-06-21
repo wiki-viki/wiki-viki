7a51185c6f25a930bbc384e39c8fcfcd570421c7
WooGi1020 <jungwk1020@gmail.com>
2024-06-21 14:26:09 +0900

    ✨ Feat: _document.tsx 파일에 meta 태그 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 const Document = () => {
   return (
     <Html lang="ko">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no" />
       <Head />
       <body>
         <Main />
