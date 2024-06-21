5eb94158ac45d2f0dfd5b6b48a9ad0fe604c9188
Lin <shl6534@naver.com>
2024-06-21 18:59:53 +0900

    ✨ Feat: 폼 태그 추가

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -9,20 +9,22 @@ import {
 const SignUpPage = () => {
   return (
     <AuthContainer title="회원가입">
-      <InputWithLabelEmail label="이름" id="nickName" placeholder="이름을 입력해 주세요." />
-      <InputWithLabelNickName label="이메일" id="email" placeholder="이메일을 입력해 주세요." />
-      <InputWithLabelPassWord
-        label="비밀번호"
-        id="password"
-        type="password"
-        placeholder="비밀번호를 입력해 주세요."
-      />
-      <InputWithLabelPassWord
-        label="비밀번호 확인"
-        id="passwordConfirmation"
-        type="password"
-        placeholder="비밀번호를 입력해 주세요."
-      />
+      <form>
+        <InputWithLabelEmail label="이름" id="nickName" placeholder="이름을 입력해 주세요." />
+        <InputWithLabelNickName label="이메일" id="email" placeholder="이메일을 입력해 주세요." />
+        <InputWithLabelPassWord
+          label="비밀번호"
+          id="password"
+          type="password"
+          placeholder="비밀번호를 입력해 주세요."
+        />
+        <InputWithLabelPassWord
+          label="비밀번호 확인"
+          id="passwordConfirmation"
+          type="password"
+          placeholder="비밀번호를 입력해 주세요."
+        />
+      </form>
       <CenterLine href="/signup" auth="로그인" />
     </AuthContainer>
   );
