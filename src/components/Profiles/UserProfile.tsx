034ab2abb6b2a4cd341b8657c6a05f6efdca9574
WooGi1020 <jungwk1020@gmail.com>
2024-06-22 16:08:24 +0900

    ✨ Feat: 위키 페이지 내부 프로필 컴포넌트 생성

---
+++ src/components/Profiles/UserProfile.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserProfile = ({ city, mbti, job, sns, birthday, nickname, bloodType, nationality }) => {
+  return (
+    <section className="profile-shadow h-[126px] w-full rounded-10 p-4">
+      <div className=""></div>
+    </section>
+  );
+};
+
+export default UserProfile;
