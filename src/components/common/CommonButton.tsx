bbe2b922fd3847c767d607d200c51d11e2eb39f7
WooGi1020 <jungwk1020@gmail.com>
2024-06-22 11:58:59 +0900

    ✨ Feat: 파라미터 기본값 지정 및 CSS 추가 설정

--- src/components/common/CommonButton.tsx
+++ src/components/common/CommonButton.tsx
@@ -1,13 +1,19 @@
-import React, { ButtonHTMLAttributes } from 'react';
+import React, { ComponentProps } from 'react';
 
-interface CommonButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface CommonButtonProps extends ComponentProps<'button'> {
   variant: 'primary' | 'secondary';
   isActive?: boolean;
   className?: string;
   children: React.ReactNode;
 }
 
-const CommonButton = ({ variant, isActive, className, children, ...props }: CommonButtonProps) => {
+const CommonButton = ({
+  variant = 'primary',
+  isActive = true,
+  className = '',
+  children,
+  ...props
+}: CommonButtonProps) => {
   // 버튼 기본 스타일링
   const baseClassName = `h-11 rounded-lg text-md-semibold px-5 py-2.5 max-w-96`;
   let variantClassName = '';
@@ -15,11 +21,11 @@ const CommonButton = ({ variant, isActive, className, children, ...props }: Comm
   switch (variant) {
     // primary에 해당하는 버튼
     case 'primary':
-      variantClassName = `flex justify-center items-center text-white ${isActive ? 'bg-primary-green-200 hover:bg-primary-green-300' : 'bg-grayscale-300'}`;
+      variantClassName = `center text-white ${isActive ? 'bg-primary-green-200 hover:bg-primary-green-300' : 'bg-grayscale-300'}`;
       break;
     // secondary에 해당하는 버튼
     case 'secondary':
-      variantClassName = `text-primary-green-200 border-2 border-primary-green-200 flex justify-center items-center`;
+      variantClassName = `text-primary-green-200 border-2 border-primary-green-200 center`;
       break;
   }
 
