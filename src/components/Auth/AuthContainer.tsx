81e64a6b6e7cdba2bf59e8e9abbb5f9b2cb46fc8
easyhyun00 <leejihyun0324@gmail.com>
2024-06-22 15:39:27 +0900

    🚑️ Fix: 빌드 오류 수정

--- src/components/Auth/AuthContainer.tsx
+++ src/components/Auth/AuthContainer.tsx
@@ -3,7 +3,7 @@ import { AuthContainerProps } from '@/types/authFormType';
 const AuthContainer = ({ title, children }: AuthContainerProps) => {
   return (
     <main className="container md:w-[335px] lg:w-[400px]">
-      <span className="block mb-[50px] text-center text-2xl-semibold text-grayscale-500">
+      <span className="mb-[50px] block text-center text-2xl-semibold text-grayscale-500">
         {title}
       </span>
       {children}
--- src/components/Auth/AuthInputWithLabel.tsx
+++ src/components/Auth/AuthInputWithLabel.tsx
@@ -1,6 +1,6 @@
+import { FieldValues } from 'react-hook-form';
 import { Label, Input } from '@/components/common/Form';
 import { InputWithLabelProps } from '@/types/authFormType';
-import { FieldValues } from 'react-hook-form';
 
 const AuthInputWithLabel = <T extends FieldValues>({
   id,
@@ -15,13 +15,13 @@ const AuthInputWithLabel = <T extends FieldValues>({
   const errorMessages = hasError ? (errors[name]?.message as string) : '';
 
   return (
-    <div className="flex flex-col gap-2.5 mb-6">
+    <div className="mb-6 flex flex-col gap-2.5">
       <Label id={id} label={label} className="text-md-regular text-grayscale-500" />
       <Input
         id={id}
         {...props}
         {...register(name, rules)}
-        className={`h-[45px] px-5 py-3.5 rounded-xl bg-grayscale-100 placeholder:text-md-regular placeholder:text-grayscale-600 ${hasError ? 'bg-secondary-red-100' : ''}`}
+        className={`h-[45px] rounded-xl bg-grayscale-100 px-5 py-3.5 placeholder:text-md-regular placeholder:text-grayscale-600 ${hasError ? 'bg-secondary-red-100' : ''}`}
       />
       {hasError && <span className="text-xs-regular text-secondary-red-200">{errorMessages}</span>}
     </div>
