e81cb52b6ec2156f6367ac121a6c1790809a5ebc
easyhyun00 <leejihyun0324@gmail.com>
2024-06-22 18:07:55 +0900

    ✨ Feat: 햄버거 메뉴 링크 이동 구현

--- src/components/common/Header/HamburgerMenuButtons.tsx
+++ src/components/common/Header/HamburgerMenuButtons.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import CommonButton from '../CommonButton';
 
 interface RenderButtonsProps {
@@ -8,6 +9,7 @@ interface RenderButtonsProps {
 type ButtonProps = {
   label: string;
   variant: 'primary' | 'secondary';
+  link: string;
   className?: string;
 };
 
@@ -15,20 +17,22 @@ const HamburgerMenuButtons = ({ isLogin }: RenderButtonsProps) => {
   const secondaryClassNames = 'bg-white hover:bg-grayscale-100';
 
   const navButtons: ButtonProps[] = [
-    { label: '위키목록', variant: 'primary' },
-    { label: '자유게시판', variant: 'primary' },
+    { label: '위키목록', variant: 'primary', link: '/wikilist' },
+    { label: '자유게시판', variant: 'primary', link: '/boards' },
   ];
 
   const userButtons: ButtonProps[] = [
     {
       label: '계정설정',
       variant: 'secondary',
+      link: '/mypage',
       className: secondaryClassNames,
     },
-    { label: '내 위키', variant: 'primary' },
+    { label: '내 위키', variant: 'primary', link: '/link/code' },
     {
       label: '로그아웃',
       variant: 'secondary',
+      link: '/logout',
       className: secondaryClassNames,
     },
   ];
@@ -37,6 +41,7 @@ const HamburgerMenuButtons = ({ isLogin }: RenderButtonsProps) => {
     {
       label: '로그인',
       variant: 'secondary',
+      link: '/login',
       className: secondaryClassNames,
     },
   ];
@@ -45,15 +50,16 @@ const HamburgerMenuButtons = ({ isLogin }: RenderButtonsProps) => {
 
   return (
     <div className="w-[180px] flex-col">
-      {buttons.map((button, index) => {
+      {buttons.map((button) => {
         return (
-          <CommonButton
-            key={index}
-            variant={button.variant}
-            className={`mt-5 w-full shadow-lg ${button.className || ''}`}
-          >
-            {button.label}
-          </CommonButton>
+          <Link href={button.link} key={button.label}>
+            <CommonButton
+              variant={button.variant}
+              className={`mt-5 w-full shadow-lg ${button.className || ''}`}
+            >
+              {button.label}
+            </CommonButton>
+          </Link>
         );
       })}
     </div>
