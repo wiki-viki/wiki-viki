8dbf4b5fd63716c5bece9a89ae9c0d3878066a57
dudwns0213 <dudwns0213@gmail.com>
2024-06-23 07:10:09 +0900

    ✨ Feat: linkbutton 구현

---
+++ src/components/common/LinkButton.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Link from 'next/link';
+import LinkIcon from '@/public/svg/link.svg';
+import { isValidUrl } from '@/utils/urlValidation';
+
+interface LinkButtonProps {
+  url: string;
+}
+
+const LinkContainer = 'inline-flex items-center gap-2 rounded-10 px-3 py-2 sm:h-6.5';
+const LinkText = 'text-md-regular sm:text-xs-regular';
+
+const LinkButton = ({ url }: LinkButtonProps) => {
+  if (!isValidUrl(url)) {
+    return (
+      <div className={`${LinkContainer} bg-secondary-red-100`}>
+        <span className={`${LinkText} truncate text-secondary-red-200`}>경로를 확인해주세요.</span>
+      </div>
+    );
+  }
+
+  return (
+    <div className={`${LinkContainer} bg-primary-green-100`}>
+      <LinkIcon />
+      <Link
+        className={`${LinkText} text-primary-green-200 `}
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {url}
+      </Link>
+    </div>
+  );
+};
+
+export default LinkButton;
