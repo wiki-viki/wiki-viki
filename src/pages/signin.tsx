f2b91dd2c33e93807076faafd7762ea092ba5ff0
Lin <shl6534@naver.com>
2024-06-22 14:36:42 +0900

    🔨 Refactor: 로그인, 회원가입 페이지 리팩토링

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,22 +1,66 @@
+import { useForm } from 'react-hook-form';
+import { AuthContainer, AuthSwitchPrompt, AuthInputWithLabel } from '@/components/Auth';
+import { DefaultFormData } from '@/types/authType';
+import { EMAIL_REGEX } from '../../constants/regex';
 import {
-  AuthContainer,
-  CenterLine,
-  InputWithLabelEmail,
-  InputWithLabelPassWord,
-} from '@/components/Auth';
+  REQUIRED_MESSAGE,
+  INVALID_EMAIL_MESSAGE,
+  PASSWORD_MIN_LENGTH_MESSAGE,
+} from '../../constants/messages';
+
+
+const emailPattern = {
+  value: EMAIL_REGEX,
+  message: INVALID_EMAIL_MESSAGE,
+};
 
 const SignInPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<DefaultFormData>();
+
+  const onSubmit = handleSubmit((data) => {
+    console.log('submitting');
+    console.log(data);
+  });
+
   return (
     <AuthContainer title="로그인">
-      <form>
-        <InputWithLabelEmail label="이름" id="nickName" placeholder="이름을 입력해 주세요." />
-        <InputWithLabelPassWord
-          label="비밀번호"
-          id="password"
-          placeholder="비밀번호를 입력해 주세요."
+      <form onSubmit={onSubmit}>
+        <AuthInputWithLabel
+          id="email"
+          name="email"
+          label="이메일"
+          type="text"
+          placeholder="이메일을 입력해 주세요."
+          register={register}
+          rules={{
+            required: REQUIRED_MESSAGE,
+            pattern: emailPattern,
+          }}
+          errors={errors}
         />
+        <AuthInputWithLabel
+          id="password"
+          name="password"
+          label="비밀번호"
+          type="password"
+          placeholder="비밀번호를 입력해 주세요."
+          register={register}
+          rules={{
+            required: REQUIRED_MESSAGE,
+            minLength: {
+              value: 8,
+              message: PASSWORD_MIN_LENGTH_MESSAGE,
+            },
+          }}
+          errors={errors}
+        />
+        <button type="submit">버튼</button>
       </form>
-      <CenterLine href="/signin" auth="로그인" />
+      <AuthSwitchPrompt href="/signin" auth="로그인" />
     </AuthContainer>
   );
 };
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,31 +1,96 @@
+import { useForm } from 'react-hook-form';
+import { AuthContainer, AuthSwitchPrompt, AuthInputWithLabel } from '@/components/Auth';
+import { DefaultFormData } from '@/types/authType';
+import { EMAIL_REGEX } from '../../constants/regex';
 import {
-  AuthContainer,
-  CenterLine,
-  InputWithLabelEmail,
-  InputWithLabelNickName,
-  InputWithLabelPassWord,
-} from '@/components/Auth';
+  REQUIRED_MESSAGE,
+  INVALID_EMAIL_MESSAGE,
+  PASSWORD_MIN_LENGTH_MESSAGE,
+  PASSWORD_MISMATCH_MESSAGE,
+} from '../../constants/messages';
+
+const emailPattern = {
+  value: EMAIL_REGEX,
+  message: INVALID_EMAIL_MESSAGE,
+};
 
 const SignUpPage = () => {
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm<DefaultFormData>();
+
+  const onSubmit = handleSubmit((data) => {
+    console.log('submitting');
+    console.log(data);
+  });
+
   return (
     <AuthContainer title="회원가입">
-      <form>
-        <InputWithLabelEmail label="이름" id="nickName" placeholder="이름을 입력해 주세요." />
-        <InputWithLabelNickName label="이메일" id="email" placeholder="이메일을 입력해 주세요." />
-        <InputWithLabelPassWord
-          label="비밀번호"
+      <form onSubmit={onSubmit}>
+        <AuthInputWithLabel
+          id="name"
+          name="name"
+          label="이름"
+          type="text"
+          placeholder="이름을 입력해 주세요."
+          register={register}
+          rules={{
+            required: REQUIRED_MESSAGE,
+          }}
+          errors={errors}
+        />
+        <AuthInputWithLabel
+          id="email"
+          name="email"
+          label="이메일"
+          type="text"
+          placeholder="이메일을 입력해 주세요."
+          register={register}
+          rules={{
+            required: REQUIRED_MESSAGE,
+            pattern: emailPattern,
+          }}
+          errors={errors}
+        />
+        <AuthInputWithLabel
           id="password"
+          name="password"
+          label="비밀번호"
           type="password"
           placeholder="비밀번호를 입력해 주세요."
+          register={register}
+          rules={{
+            required: REQUIRED_MESSAGE,
+            minLength: {
+              value: 8,
+              message: PASSWORD_MIN_LENGTH_MESSAGE,
+            },
+          }}
+          errors={errors}
         />
-        <InputWithLabelPassWord
-          label="비밀번호 확인"
+        <AuthInputWithLabel
           id="passwordConfirmation"
+          name="passwordConfirmation"
+          label="비밀번호 확인"
           type="password"
           placeholder="비밀번호를 입력해 주세요."
+          register={register}
+          rules={{
+            required: REQUIRED_MESSAGE,
+            minLength: {
+              value: 8,
+              message: PASSWORD_MIN_LENGTH_MESSAGE,
+            },
+            validate: (value) => value === getValues('password') || PASSWORD_MISMATCH_MESSAGE,
+          }}
+          errors={errors}
         />
+        <button type="submit">버튼</button>
       </form>
-      <CenterLine href="/signup" auth="로그인" />
+      <AuthSwitchPrompt href="/signup" auth="로그인" />
     </AuthContainer>
   );
 };
