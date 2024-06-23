0642efa15725fa533655670e94037bb0b77abc3a
dudwns0213 <dudwns0213@gmail.com>
2024-06-23 18:33:40 +0900

    ✨ Feat: toastoptions type 지정 및 기본값 정의

---
+++ src/types/toastOptions.ts
@@ -0,0 +1,12 @@
+import { ToastOptions } from 'react-toastify';
+
+export const toastOptions: ToastOptions = {
+  position: 'top-center',
+  autoClose: 2000,
+  hideProgressBar: true,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  pauseOnFocusLoss: true,
+  closeButton: false,
+};
