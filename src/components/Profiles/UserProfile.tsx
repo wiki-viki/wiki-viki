86bd2ce5d8fa37708d38f10014e9dcd37fc35ca6
WooGi1020 <jungwk1020@gmail.com>
2024-06-23 23:55:05 +0900

    ✨ Feat: 프로필 파일 생성 및 로직 작성

--- src/components/Profiles/UserProfile.tsx
+++ src/components/Profiles/UserProfile.tsx
@@ -1,9 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Image from 'next/image';
+import ProfileInfos from './ProfileInfos';
 
-const UserProfile = ({ city, mbti, job, sns, birthday, nickname, bloodType, nationality }) => {
+interface UserProfileProps {
+  nationality: string;
+  bloodType: string;
+  nickname: string;
+  birthday: string;
+  sns: string;
+  job: string;
+  mbti: string;
+  city: string;
+  image: string;
+}
+
+const UserProfile: React.FC<UserProfileProps> = ({
+  image,
+  city,
+  mbti,
+  job,
+  sns,
+  birthday,
+  nickname,
+  bloodType,
+  nationality,
+}) => {
+  const profileFields = [
+    { label: '거주 도시', value: city },
+    { label: 'MBTI', value: mbti },
+    { label: '직업', value: job },
+    { label: 'SNS 계정', value: sns },
+    { label: '생일', value: birthday },
+    { label: '별명', value: nickname },
+    { label: '혈액형', value: bloodType },
+    { label: '국적', value: nationality },
+  ];
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleProfileExtend = () => {
+    setIsExpanded((prev) => {
+      return !prev;
+    });
+  };
+
+  const profileHeight = isExpanded ? `h-fit` : `h-[70px] md:h-[85px] xl:h-fit`;
+
   return (
-    <section className="profile-shadow h-[126px] w-full rounded-10 p-4">
-      <div className=""></div>
+    <section
+      className={`profile-shadow xl:center flex w-full flex-col justify-start rounded-10 bg-white p-5 xl:sticky xl:top-[30px] xl:ml-auto xl:h-[671px] xl:w-[320px] xl:p-10`}
+    >
+      <div className="flex w-full xl:flex-col">
+        <div className="relative mr-4 size-[62px] rounded-full border border-grayscale-200 md:mr-10 md:size-[81px] xl:mx-auto xl:mb-12 xl:size-[200px]">
+          <Image
+            src={image}
+            sizes="(max-width: 768px) 62px, (max-width: 1200px) 81px, 200px"
+            fill
+            priority
+            className="rounded-full object-cover"
+            alt="위키 페이지 개인 프로필 이미지"
+          />
+        </div>
+        <div className={`mb-5 flex flex-col gap-2 overflow-hidden xl:gap-4 ${profileHeight}`}>
+          {profileFields.map((field) => {
+            return <ProfileInfos key={field.label} {...field} />;
+          })}
+        </div>
+      </div>
+
+      <div className="flex cursor-pointer justify-center" onClick={handleProfileExtend}>
+        <p className=" text-md-regular xl:hidden">버튼이다</p>
+      </div>
     </section>
   );
 };
