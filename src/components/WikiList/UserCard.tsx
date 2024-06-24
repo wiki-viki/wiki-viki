8b788027b7408cbd4d12a95ecda22857b6afd4d4
dudwns0213 <dudwns0213@gmail.com>
2024-06-25 05:14:23 +0900

    ✨ Feat: wikilist에 사용되는 card 구현 및 스타일링

---
+++ src/components/WikiList/UserCard.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { cardType } from '@/types/UserCardProps';
+import { WIKI_BASE_URL } from '@/constants/url';
+import LinkButton from '../common/LinkButton';
+
+interface CardListProps {
+  cardList: cardType[];
+}
+
+const UserCard = ({ cardList }: CardListProps) => {
+  const router = useRouter();
+  const { code } = router.query;
+  const URL = `${WIKI_BASE_URL}${code}`;
+
+  return (
+    <div className="relative mt-14 h-3/5 w-full">
+      {cardList.map((card) => {
+        return (
+          <motion.div
+            key={card.id}
+            className="card"
+            whileHover={{ scale: 1.02 }}
+            transition={{ duration: 0.15 }}
+          >
+            <Link href={`/wiki/${card.id}`} rel="preload">
+              <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                  <Image
+                    className="rounded-full border-2 sm:size-[60px]"
+                    src={card.image}
+                    alt="프로필 이미지"
+                    width={85}
+                    height={85}
+                  />
+
+                  <div className="ml-8 text-grayscale-400 ">
+                    <h3 className="truncate text-2xl-semibold text-grayscale-500 sm:text-xl-semibold">
+                      {card.name}
+                    </h3>
+                    <div className="mt-4 text-md-regular sm:mt-[10px] sm:text-xs-regular">
+                      {card.city}, {card.nationality}
+                      <div>{card.job}</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Link>
+            <div className="absolute bottom-0 right-0 mb-6 mr-9 ">
+              <LinkButton url={URL} />
+            </div>
+          </motion.div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default UserCard;
