3adfc2e0b30d4f9a00e923db9bd6c4332ea12e9f
dudwns0213 <dudwns0213@gmail.com>
2024-06-25 05:30:32 +0900

    ✨ Feat: searchlabel 구현

---
+++ src/components/WikiList/SearchLabel.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/no-unescaped-entities */
+
+interface NoSearchProps {
+  keyword: string;
+  totalCount: number;
+}
+
+const SearchLabel = ({ keyword, totalCount }: NoSearchProps) => {
+  return (
+    <div className={`mt-4 text-md-regular text-grayscale-400 ${keyword ? '' : 'invisible'}`}>
+      "{keyword}" 님을 총 <span className=" text-primary-green-200">{totalCount}명</span>{' '}
+      찾았습니다.
+    </div>
+  );
+};
+
+export default SearchLabel;
