12d4028a1600a7fdf99b2bfb552433a40f686813
WooGi1020 <jungwk1020@gmail.com>
2024-06-24 14:02:24 +0900

    ✨ Feat: Framer motion 사용하여 애니메이션 추가

--- src/components/Profiles/UserProfile.tsx
+++ src/components/Profiles/UserProfile.tsx
@@ -1,18 +1,10 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
+import { motion, AnimatePresence } from 'framer-motion';
+import { UserProfileProps } from '@/types/UserProfileProps';
+import useIsMobile from '@/hooks/useIsMobile';
+import ExpandIcon from '../../../public/svg/profile_expand_icon.svg';
 import ProfileInfos from './ProfileInfos';
-
-interface UserProfileProps {
-  nationality: string;
-  bloodType: string;
-  nickname: string;
-  birthday: string;
-  sns: string;
-  job: string;
-  mbti: string;
-  city: string;
-  image: string;
-}
 
 const UserProfile: React.FC<UserProfileProps> = ({
   image,
@@ -44,33 +36,50 @@ const UserProfile: React.FC<UserProfileProps> = ({
   };
 
   const profileHeight = isExpanded ? `h-fit` : `h-[70px] md:h-[85px] xl:h-fit`;
+  const mobileSize = useIsMobile();
 
   return (
-    <section
-      className={`profile-shadow xl:center flex w-full flex-col justify-start rounded-10 bg-white p-5 xl:sticky xl:top-[30px] xl:ml-auto xl:h-[671px] xl:w-[320px] xl:p-10`}
-    >
-      <div className="flex w-full xl:flex-col">
-        <div className="relative mr-4 size-[62px] rounded-full border border-grayscale-200 md:mr-10 md:size-[81px] xl:mx-auto xl:mb-12 xl:size-[200px]">
-          <Image
-            src={image}
-            sizes="(max-width: 768px) 62px, (max-width: 1200px) 81px, 200px"
-            fill
-            priority
-            className="rounded-full object-cover"
-            alt="위키 페이지 개인 프로필 이미지"
-          />
-        </div>
-        <div className={`mb-5 flex flex-col gap-2 overflow-hidden xl:gap-4 ${profileHeight}`}>
-          {profileFields.map((field) => {
-            return <ProfileInfos key={field.label} {...field} />;
-          })}
-        </div>
-      </div>
+    <AnimatePresence>
+      <section
+        className={`profile-shadow xl:center flex w-full flex-col justify-start rounded-10 bg-white p-5 xl:sticky xl:top-[110px] xl:ml-auto xl:h-[671px] xl:w-[320px] xl:p-10`}
+      >
+        <div className="flex w-full xl:flex-col">
+          <div className="relative mr-4 size-[71px] rounded-full border border-grayscale-200 md:mr-10 md:size-[81px] xl:mx-auto xl:mb-12 xl:size-[200px]">
+            <Image
+              src={image}
+              sizes="(max-width: 768px) 62px, (max-width: 1200px) 81px, 200px"
+              fill
+              priority
+              className="rounded-full object-cover"
+              alt="위키 페이지 개인 프로필 이미지"
+            />
+          </div>
 
-      <div className="flex cursor-pointer justify-center" onClick={handleProfileExpand}>
-        <p className=" text-md-regular xl:hidden">버튼이다</p>
-      </div>
-    </section>
+          <motion.div
+            className="xl:mb-[210px]"
+            animate={{
+              height: isExpanded ? (mobileSize ? `215px` : `260px`) : mobileSize ? `85px` : '100px',
+            }}
+            transition={{ duration: 0.15 }}
+          >
+            <div className={`mb-5 flex flex-col gap-2 overflow-hidden xl:gap-4 ${profileHeight}`}>
+              {profileFields.map((field) => {
+                return <ProfileInfos key={field.label} {...field} />;
+              })}
+            </div>
+          </motion.div>
+        </div>
+
+        <motion.div
+          className="flex cursor-pointer justify-center xl:hidden"
+          onClick={handleProfileExpand}
+          animate={{ rotate: isExpanded ? 180 : 0 }}
+          transition={{ duration: 0.2 }}
+        >
+          <ExpandIcon />
+        </motion.div>
+      </section>
+    </AnimatePresence>
   );
 };
 
