cf7261e3d28ef1ebce8abadd48af65ac0b02d460
WooGi1020 <jungwk1020@gmail.com>
2024-06-25 19:17:35 +0900

    ✨ Feat: article 관련 데이터 받아올 API 함수 작성

---
+++ src/lib/apis/article/articleApi.api.ts
@@ -0,0 +1,133 @@
+import axios, { AxiosResponse } from 'axios';
+import { ArticleResponse, ArticleListResponse, ArticleFormData } from '@/types/apiType';
+import axiosWithIntercepter from '../axiosWithIntercepter';
+import axiosDefault from '../axiosDefault';
+
+// 게시글 목록 GET 요청
+export const getArticle = async ({
+  page = 1,
+  pageSize = 10,
+  orderBy = 'resent',
+  keyword = '',
+}: {
+  page?: number;
+  pageSize?: number;
+  orderBy?: string;
+  keyword?: string;
+}) => {
+  const params = new URLSearchParams({
+    page: page.toString(),
+    pageSize: pageSize.toString(),
+    orderBy: orderBy,
+    keyword: keyword,
+  });
+
+  try {
+    const res: AxiosResponse<ArticleListResponse> = await axiosDefault.get(`articles?${params}`);
+    return res.data;
+  } catch (e: unknown) {
+    if (axios.isAxiosError(e)) {
+      throw e.response?.data.message;
+    } else {
+      throw new Error('예상치 못한 오류가 발생하였습니다. 관리자에게 문의하세요!');
+    }
+  }
+};
+
+// 게시글 상세 페이지 GET 요청
+export const getDetailArticle = async (articleId: number) => {
+  try {
+    const res: AxiosResponse<ArticleResponse> = await axiosWithIntercepter.get(
+      `articles/${articleId}`,
+    );
+    return res.data;
+  } catch (e: unknown) {
+    if (axios.isAxiosError(e)) {
+      throw e.response?.data.message;
+    } else {
+      throw new Error('예상치 못한 오류가 발생하였습니다. 관리자에게 문의하세요!');
+    }
+  }
+};
+
+// 게시글 상세 페이지 POST 요청
+export const postArticle = async (formData: ArticleFormData) => {
+  try {
+    const res: AxiosResponse<ArticleResponse> = await axiosWithIntercepter.post(
+      `articles`,
+      formData,
+    );
+    return res.data;
+  } catch (e: unknown) {
+    if (axios.isAxiosError(e)) {
+      throw e.response?.data.message;
+    } else {
+      throw new Error('예상치 못한 오류가 발생하였습니다. 관리자에게 문의하세요!');
+    }
+  }
+};
+
+// 게시글 상세 페이지 PATCH 요청
+export const changeDetailArticle = async (articleId: number, formData: ArticleFormData) => {
+  try {
+    const res: AxiosResponse<ArticleResponse> = await axiosWithIntercepter.patch(
+      `articles/${articleId}`,
+      formData,
+    );
+    return res.data;
+  } catch (e: unknown) {
+    if (axios.isAxiosError(e)) {
+      throw e.response?.data.message;
+    } else {
+      throw new Error('예상치 못한 오류가 발생하였습니다. 관리자에게 문의하세요!');
+    }
+  }
+};
+
+// // 게시글 상세 페이지 DELETE 요청
+export const deleteDetailArticle = async (articleId: number) => {
+  try {
+    const res: AxiosResponse<ArticleResponse> = await axiosWithIntercepter.delete(
+      `articles/${articleId}`,
+    );
+    return res.data;
+  } catch (e: unknown) {
+    if (axios.isAxiosError(e)) {
+      throw e.response?.data.message;
+    } else {
+      throw new Error('예상치 못한 오류가 발생하였습니다. 관리자에게 문의하세요!');
+    }
+  }
+};
+
+// 게시글 상세 페이지 좋아요 POST 요청
+export const postArticleLike = async (articleId: number) => {
+  try {
+    const res: AxiosResponse<ArticleResponse> = await axiosWithIntercepter.post(
+      `articles/${articleId}`,
+    );
+    return res.data;
+  } catch (e: unknown) {
+    if (axios.isAxiosError(e)) {
+      throw e.response?.data.message;
+    } else {
+      throw new Error('예상치 못한 오류가 발생하였습니다. 관리자에게 문의하세요!');
+    }
+  }
+};
+
+// 게시글 상세 페이지 좋아요 DELETE 요청
+export const deleteArticleLike = async (articleId: number) => {
+  try {
+    const res: AxiosResponse<ArticleResponse> = await axiosWithIntercepter.delete(
+      `articles/${articleId}`,
+    );
+    return res.data;
+  } catch (e: unknown) {
+    if (axios.isAxiosError(e)) {
+      throw e.response?.data.message;
+    } else {
+      throw new Error('예상치 못한 오류가 발생하였습니다. 관리자에게 문의하세요!');
+    }
+  }
+};
