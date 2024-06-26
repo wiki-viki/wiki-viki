f185471dbbcc457ce1f378f424af650269a066bc
WooGi1020 <jungwk1020@gmail.com>
2024-06-26 18:03:08 +0900

    ✨ Feat: isEditing 상태에 따른 페이지 변경

--- src/pages/wiki/[code].tsx
+++ src/pages/wiki/[code].tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import UserProfile from '@/components/Profiles/UserProfile';
 import CommonButton from '@/components/common/CommonButton';
@@ -17,39 +17,70 @@ const UserWikiPage: React.FC = () => {
   const router = useRouter();
   const { code } = router.query;
   const URL = `${WIKI_BASE_URL}${code}`;
+  const [isEditing, setIsEditing] = useState(false);
 
   const { value, handleOff, handleOn } = useBoolean();
+
+  const hadnleWikiStateButtonClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleCancelClick = () => {
+    setIsEditing(false);
+  };
+
+  const handleSaveClick = () => {};
 
   return (
     <div className="center m-auto max-w-[1350px] flex-col px-6 py-14 sm:flex-col sm:pt-10 md:px-14 xl:relative">
       <StyledToastContainer limit={1} />
 
-      <div className="mb-5 flex w-full items-center justify-between">
-        <h1 className="text-3_5xl-bold text-grayscale-500">{mockData.name}</h1>
-        {mockData.content && (
-          <CommonButton variant="primary" className="xl:mr-[385px]" onClick={handleOn}>
-            위키 참여하기
-          </CommonButton>
-        )}
-      </div>
+      {isEditing || (
+        <>
+          <div className="mb-5 flex w-full items-center justify-between">
+            <h1 className="text-3_5xl-bold text-grayscale-500">{mockData.name}</h1>
+            {mockData.content && (
+              <CommonButton variant="primary" className="xl:mr-[385px]" onClick={handleOn}>
+                위키 참여하기
+              </CommonButton>
+            )}
+          </div>
+          <div className="mb-5 flex w-full justify-start">
+            <CopyLinkButton url={URL} />
+          </div>
+        </>
+      )}
 
-      <div className="mb-5 flex w-full justify-start">
-        <CopyLinkButton url={URL} />
-      </div>
-
-      <UserProfile {...mockData} />
+      <UserProfile {...mockData} isEditing={isEditing} />
 
       <div className="w-full xl:absolute xl:bottom-[500px] xl:right-[440px] xl:mb-auto xl:w-[856px]">
-        {mockData.content === null ? (
-          <div className="flex h-[184px] w-full flex-col items-center justify-center rounded-10 bg-grayscale-100 md:mt-14 md:h-[192px]">
-            <p className={noContentClassName}>아직 작성된 내용이 없네요.</p>
-            <p className={noContentClassName}>위키에 참여해보세요!</p>
-            <CommonButton variant="primary" className="mt-4" onClick={handleOn}>
-              시작하기
-            </CommonButton>
-          </div>
-        ) : null}
+        {mockData.content === null
+          ? isEditing || (
+              <div className="flex h-[184px] w-full flex-col items-center justify-center rounded-10 bg-grayscale-100 md:mt-14 md:h-[192px]">
+                <p className={noContentClassName}>아직 작성된 내용이 없네요.</p>
+                <p className={noContentClassName}>위키에 참여해보세요!</p>
+                <CommonButton
+                  variant="primary"
+                  className="mt-4"
+                  onClick={hadnleWikiStateButtonClick}
+                >
+                  시작하기
+                </CommonButton>
+              </div>
+            )
+          : null}
       </div>
+
+      {isEditing && (
+        <div className="ml-auto mt-auto flex gap-3 sm:absolute sm:right-[45px] sm:top-[75px] md:absolute md:right-[75px] md:top-[75px] lg:right-[75px] lg:top-[95px] xl:static">
+          <CommonButton variant="secondary" onClick={handleCancelClick}>
+            취소
+          </CommonButton>
+          <CommonButton variant="primary" onClick={handleSaveClick}>
+            저장
+          </CommonButton>
+        </div>
+      )}
 
       <Modal isOpen={value} onClose={handleOff}>
         <QuizModalTemplete onClose={handleOff} />
