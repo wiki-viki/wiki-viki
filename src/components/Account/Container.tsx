2ad48d5f9e81f0ac2e7d220f5357dc8d9f944f74
Lin <shl6534@naver.com>
2024-06-26 14:20:44 +0900

    📦️ Chore: ESLint 설정에 따른 변경사항

--- src/components/Account/Container.tsx
+++ src/components/Account/Container.tsx
@@ -3,7 +3,7 @@ import { AuthContainerProps } from '@/types/authFormType';
 const Container = ({ title, children }: AuthContainerProps) => {
   return (
     <main className="centerOfScreen">
-      <div className="md:w-[335px] lg:w-[400px] mt-[70px]">
+      <div className="mt-[70px] md:w-[335px] lg:w-[400px]">
         <span className="mb-[50px] block text-center text-2xl-semibold text-grayscale-500">
           {title}
         </span>
