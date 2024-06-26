c7b06f851964edf3e634172a28a5d0da7708741a
Lin <shl6534@naver.com>
2024-06-26 14:19:57 +0900

    ✨ Feat: 미들웨어 구현

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export const middleware = (request: NextRequest) => {
+  const accessToken = request.cookies.get('accessToken');
+  const { pathname } = request.nextUrl;
+
+  if (pathname === '/login' && accessToken) {
+    return NextResponse.redirect(new URL('/', request.nextUrl));
+  } else if (pathname === '/signup' && accessToken) {
+    return NextResponse.redirect(new URL('/', request.nextUrl));
+  } else if (pathname !== '/login' && pathname !== '/signup' && !accessToken) {
+    return NextResponse.redirect(new URL('/login', request.nextUrl));
+  }
+};
+
+export const config = {
+  matcher: [
+    '/login',
+    '/signup',
+    '/mypage',
+    '/wiki/:path*',
+    '/wikilist',
+    '/boards/:path*',
+    '/addboards',
+    '/testinterceptor'
+  ],
+};
