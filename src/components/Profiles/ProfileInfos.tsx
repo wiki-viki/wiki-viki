cdfe42e70a4eca77140f01a771406b1f90d410c2
WooGi1020 <jungwk1020@gmail.com>
2024-06-26 18:03:02 +0900

    ✨ Feat: isEditing 상태에 따른 페이지 변경

--- src/components/Profiles/ProfileInfos.tsx
+++ src/components/Profiles/ProfileInfos.tsx
@@ -1,15 +1,38 @@
 import React, { memo } from 'react';
+import { Input, Label } from '../common/Form';
 
-const ProfileInfos = memo(({ label, value }: { label: string; value: string }) => {
-  return (
-    <div className="flex w-fit gap-[10px]">
-      <p className="min-w-14 flex-none text-xs-regular text-grayscale-400 md:text-md-regular xl:min-w-16">
-        {label}
-      </p>
-      <p className="text-xs-regular text-grayscale-500 md:text-md-regular">{value}</p>
-    </div>
-  );
-});
+const ProfileInfos = memo(
+  ({ label, value, isEditing }: { label: string; value: string; isEditing: boolean }) => {
+    return (
+      <div
+        className={`flex h-7 w-full items-center gap-[10px] ${isEditing ? 'md:mx-auto xl:mb-1' : ''}`}
+      >
+        {isEditing ? (
+          <>
+            <Label
+              htmlFor="profileInput"
+              className="min-w-14 flex-none text-xs-regular text-grayscale-400 md:text-md-regular xl:min-w-16"
+              label={label}
+            />
+            <Input
+              name="profileInput"
+              id="profileInput"
+              className="flex-none rounded-10 bg-grayscale-100 py-2 pl-3 text-xs-regular text-grayscale-400 outline-none focus:border-2 focus:border-primary-green-200 sm:w-[70%] md:ml-2 md:w-4/5 md:text-md-regular lg:ml-0 xl:w-[68%] xl:min-w-16"
+              placeholder={value}
+            />
+          </>
+        ) : (
+          <>
+            <p className="min-w-14 flex-none text-xs-regular text-grayscale-400 md:text-md-regular xl:min-w-16">
+              {label}
+            </p>
+            <p className="text-xs-regular text-grayscale-500 md:text-md-regular">{value}</p>
+          </>
+        )}
+      </div>
+    );
+  },
+);
 
 ProfileInfos.displayName = 'ProfileInfos';
 
