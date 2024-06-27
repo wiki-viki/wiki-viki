006520fa08d5f9a2cd0a36fea2f5fb2fc7203ad2
WooGi1020 <jungwk1020@gmail.com>
2024-06-27 23:33:51 +0900

    ✨ Feat: 에디터 적용 및 자잘한 CSS 코드 수정

--- src/pages/wiki/[code].tsx
+++ src/pages/wiki/[code].tsx
@@ -9,6 +9,8 @@ import CopyLinkButton from '@/components/common/CopyLinkButton';
 import { WIKI_BASE_URL } from '@/constants/url';
 import { StyledToastContainer } from '@/styles/ToastStyle';
 import 'react-toastify/dist/ReactToastify.css';
+import { Editor, EditorMarkdown } from '@/components/Profiles/Editor';
+import { EDITOR_TEXT } from '@/constants/editorBasicText';
 import mockData from '../../../public/profileMockData.json';
 
 const noContentClassName = `text-lg-regular text-grayscale-400`;
@@ -18,10 +20,29 @@ const UserWikiPage: React.FC = () => {
   const { code } = router.query;
   const URL = `${WIKI_BASE_URL}${code}`;
   const [isEditing, setIsEditing] = useState(false);
+  const [md, setMD] = useState<string | undefined>(
+    mockData.content ? mockData.content : EDITOR_TEXT,
+  );
+
+  const contentClassName = `
+  w-full xl:absolute
+  ${mockData.content ? 'xl:top-[200px] break-words' : 'xl:bottom-[500px]'}
+  ${isEditing ? 'xl:top-[37px]' : ''}
+  md:mt-5 xl:right-[440px] xl:w-[856px]
+  ${mockData.content && isEditing ? 'xl:top-[40px]' : ''}
+`.trim();
 
   const { value, handleOff, handleOn } = useBoolean();
 
   const hadnleWikiStateButtonClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleEditorChange = (value: string | undefined) => {
+    setMD(value);
+  };
+
+  const setEditingMode = () => {
     setIsEditing(true);
   };
 
@@ -32,9 +53,8 @@ const UserWikiPage: React.FC = () => {
   const handleSaveClick = () => {};
 
   return (
-    <div className="center m-auto max-w-[1350px] flex-col px-6 py-14 sm:flex-col sm:pt-10 md:px-14 xl:relative">
+    <div className="center xl:h-auto m-auto max-w-[1350px] flex-col px-6 py-14 sm:flex-col sm:pt-10 md:px-14 xl:relative">
       <StyledToastContainer limit={1} />
-
       {isEditing || (
         <>
           <div className="mb-5 flex w-full items-center justify-between">
@@ -53,30 +73,31 @@ const UserWikiPage: React.FC = () => {
 
       <UserProfile {...mockData} isEditing={isEditing} />
 
-      <div
-        className={`w-full xl:absolute ${mockData.content ? 'xl:top-[200px]' : 'xl:bottom-[500px]'} md:mt-5 xl:right-[440px] xl:mb-auto xl:w-[856px]`}
-      >
-        {mockData.content === null
-          ? isEditing || (
-              <div className="flex h-[184px] w-full flex-col items-center justify-center rounded-10 bg-grayscale-100 md:mt-5 md:h-[192px]">
-                <p className={noContentClassName}>아직 작성된 내용이 없네요.</p>
-                <p className={noContentClassName}>위키에 참여해보세요!</p>
-                <CommonButton
-                  variant="primary"
-                  className="mt-4"
-                  onClick={hadnleWikiStateButtonClick}
-                >
-                  시작하기
-                </CommonButton>
-              </div>
-            )
-          : isEditing
-            ? ''
-            : mockData.content}
+      <div className={contentClassName}>
+        {!mockData.content && !isEditing && (
+          <div className="flex h-[184px] w-full flex-col items-center justify-center rounded-10 bg-grayscale-100 md:mt-5 md:h-[192px]">
+            <p className={noContentClassName}>아직 작성된 내용이 없네요.</p>
+            <p className={noContentClassName}>위키에 참여해보세요!</p>
+            <CommonButton variant="primary" className="mt-4" onClick={hadnleWikiStateButtonClick}>
+              시작하기
+            </CommonButton>
+          </div>
+        )}
+        {isEditing ? (
+          <Editor
+            preview="live"
+            value={md}
+            onChange={handleEditorChange}
+            height={700}
+            toolbarHeight={50}
+          />
+        ) : (
+          <EditorMarkdown source={mockData.content} />
+        )}
       </div>
 
       {isEditing && (
-        <div className="ml-auto mt-auto flex gap-3 sm:absolute sm:right-[45px] sm:top-[75px] md:absolute md:right-[75px] md:top-[75px] lg:right-[75px] lg:top-[95px] xl:static xl:mt-5">
+        <div className="ml-auto flex gap-3 sm:absolute sm:right-[45px] sm:top-[75px] md:absolute md:right-[75px] md:top-[75px] lg:right-[75px] lg:top-[95px] xl:static xl:mt-[30px]">
           <CommonButton variant="secondary" onClick={handleCancelClick}>
             취소
           </CommonButton>
@@ -87,7 +108,7 @@ const UserWikiPage: React.FC = () => {
       )}
 
       <Modal isOpen={value} onClose={handleOff}>
-        <QuizModalTemplete onClose={handleOff} />
+        <QuizModalTemplete onClose={handleOff} setEditingMode={setEditingMode} />
       </Modal>
     </div>
   );
