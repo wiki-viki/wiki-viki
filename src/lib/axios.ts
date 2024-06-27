b711f8bbfff86dd39fd359668f6d767cf1d50b13
dudwns0213 <dudwns0213@gmail.com>
2024-06-27 12:05:07 +0900

    🚚 Rename: url 이름 오류 수정

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,17 +1,20 @@
 import axios, { AxiosRequestConfig, AxiosResponse, AxiosRequestHeaders } from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_KEY,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 10_000,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-type AxiosRequester = <T>(options: AxiosRequestConfig<T>, includeAuth?: boolean) => Promise<AxiosResponse<T>>;
+type AxiosRequester = <T>(
+  options: AxiosRequestConfig<T>,
+  includeAuth?: boolean,
+) => Promise<AxiosResponse<T>>;
 
 export const axiosRequester: AxiosRequester = async (options, includeAuth) => {
-  const headers  = { ...options.headers } as AxiosRequestHeaders;
+  const headers = { ...options.headers } as AxiosRequestHeaders;
 
   if (includeAuth) {
     const accessToken = document.cookie
