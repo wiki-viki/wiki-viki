43f1b25c2c3a4cae934e8f26fc85f63b8a1de59b
easyhyun00 <leejihyun0324@gmail.com>
2024-06-27 16:41:40 +0900

    🔨 Refactor: public import 경로 수정

--- src/components/common/TopNavigationBar/index.tsx
+++ src/components/common/TopNavigationBar/index.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import useBoolean from '@/hooks/useBoolean';
 import { useUserStore } from '@/store/userStore';
-import Logo from '../../../../public/svg/wiki-viki-logo.svg';
-import NotifyIcon from '../../../../public/svg/notification.svg';
-import ProfileIcon from '../../../../public/svg/profile.svg';
-import HamburgerIcon from '../../../../public/svg/hamburger.svg';
+import Logo from '@/../public/svg/wiki-viki-logo.svg';
+import NotifyIcon from '@/../public/svg/notification.svg';
+import ProfileIcon from '@/../public/svg/profile.svg';
+import HamburgerIcon from '@/../public/svg/hamburger.svg';
 import UserMenu from './UserMenu';
 import AuthUserMenu from './AuthUserMenu';
 import NoticeMenu from './NoticeMenu';
@@ -58,17 +58,17 @@ const TopNavigationBar = () => {
   return (
     <header className="fixed z-20 flex h-[60px] w-full items-center justify-between border-b-grayscale-300 bg-white px-5 shadow-md lg:h-[80px] lg:px-[80px]">
       <div className="flex items-center gap-5">
-        <Link href="/">
+        <Link href="/" rel="preload">
           <Logo width={107} height={30} />
         </Link>
         <nav className="hidden md:block">
           <ul className="flex gap-5 text-md-regular text-grayscale-500 ">
-            <Link href="/wikilist" className={linkClassNames}>
+            <Link href="/wikilist" className={linkClassNames} rel="preload">
               <li className={`${pathname === '/wikilist' ? activeLinkClassNames : ''}`}>
                 위키목록
               </li>
             </Link>
-            <Link href="/boards" className={linkClassNames}>
+            <Link href="/boards" className={linkClassNames} rel="preload">
               <li className={`${pathname === '/boards' ? activeLinkClassNames : ''}`}>
                 자유게시판
               </li>
@@ -97,6 +97,7 @@ const TopNavigationBar = () => {
         ) : (
           <>
             <Link
+              rel="preload"
               href="/login"
               className={`${linkClassNames} hidden py-1 text-md-regular text-grayscale-400 md:block`}
             >
