f821a54fe433f91a37ee72e461fcaaa5442b67d6
easyhyun00 <leejihyun0324@gmail.com>
2024-06-28 16:42:45 +0900

    ✨ Feat: 게시물 등록하기 태블릿/PC UI 구현(본문작성 제외)

--- src/pages/addboard.tsx
+++ src/pages/addboard.tsx
@@ -9,12 +9,16 @@ const AddBoard = () => {
 
   return (
     <div className="center mt-4 flex-col">
-      <main className="flex w-full flex-col gap-3">
+      <main className="md:profile-shadow flex w-full max-w-[1060px] flex-col gap-3 rounded-10 md:gap-5 md:px-[30px] md:py-[40px]">
         <div className="flex items-center justify-between">
-          <h2 className="text-lg-semibold">게시물 등록하기</h2>
+          <h2 className="text-lg-semibold md:text-xl-semibold lg:text-2xl-semibold">
+            게시물 등록하기
+          </h2>
           <CommonButton variant="primary">등록하기</CommonButton>
         </div>
-        <span className="text-xs-regular text-gray-400">작성일 {dateToString(new Date())}</span>
+        <span className="text-xs-regular text-gray-400 md:text-lg-regular">
+          등록일 {dateToString(new Date())}
+        </span>
         <div>
           <div className="mt-1 border-t" />
           <div className="flex items-center justify-between gap-2">
@@ -23,17 +27,19 @@ const AddBoard = () => {
                 setTitle(e.target.value);
               }}
               value={title}
-              className="flex-1 rounded-sm py-3 text-lg-medium outline-none"
+              className="flex-1 rounded-sm py-3 text-lg-medium outline-none md:text-xl-medium"
               placeholder="제목을 입력해주세요"
               maxLength={TITLE_MAX_LEN}
             />
-            <span className="text-xs-medium">
+            <span className="text-xs-medium md:text-md-medium">
               {title.length}/<span className="text-primary-green-200">{TITLE_MAX_LEN}</span>
             </span>
           </div>
           <div className="border-t" />
         </div>
-        <span className="text-md-medium">공백포함 : 총 0자 | 공백제외: 총 0자</span>
+        <span className="text-md-medium md:text-lg-medium">
+          공백포함 : 총 0자 | 공백제외: 총 0자
+        </span>
       </main>
       <CommonButton variant="secondary">목록으로</CommonButton>
     </div>
