1a258b19393f23527094bd5a2aa73d9eaf983cfc
WooGi1020 <jungwk1020@gmail.com>
2024-06-29 18:23:45 +0900

    ✨ Feat: formData 기본값 추가

---
+++ src/constants/formDataInitialValue.ts
@@ -0,0 +1,15 @@
+export const FORM_DATA_INIT = {
+  securityAnswer: '',
+  securityQuestion: '',
+  nationality: '',
+  family: '',
+  bloodType: '',
+  nickname: '',
+  birthday: '',
+  sns: '',
+  job: '',
+  mbti: '',
+  city: '',
+  image: null,
+  content: '',
+};
