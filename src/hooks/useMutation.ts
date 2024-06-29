109d8414f4cf353ec7f440c4648df539c0cc0b17
Lin <shl6534@naver.com>
2024-06-29 18:12:28 +0900

    ✨ Feat: useMutation 훅 생성

---
+++ src/hooks/useMutation.ts
@@ -0,0 +1,49 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState } from 'react';
+import { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
+import { axiosRequester } from '@/lib/axios';
+
+interface RequestConfig {
+  options: AxiosRequestConfig<any>;
+  deps?: any[];
+  includeAuth?: boolean;
+}
+
+type AxiosFetch = <T>(options?: AxiosRequestConfig<T>) => Promise<T | any>;
+
+const useMutation = <T>({ options, includeAuth = false }: RequestConfig) => {
+  const [data, setData] = useState<AxiosResponse<T> | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState<string | null>(null);
+  const [statusCode, setStatusCode] = useState<number | null>(null);
+
+  const axiosFetch: AxiosFetch = async (args) => {
+    setIsLoading(true);
+    setIsError(null);
+    setStatusCode(null);
+
+    try {
+      const response = await axiosRequester({ ...options, ...args }, includeAuth);
+      const accessToken = response?.data?.accessToken;
+
+      if (includeAuth && accessToken) {
+        document.cookie = `accessToken=${accessToken}`;
+      }
+
+      setData(response);
+      return response;
+    } catch (err) {
+      if (err instanceof AxiosError) {
+        setIsError(err.response?.data?.message || err.message);
+        setStatusCode(err.response?.status || null);
+      }
+      return err;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { data, isLoading, isError, statusCode, axiosFetch };
+};
+
+export default useMutation;
