86020dd32ae95fd1dc24bd94e90da604fe6a9354
WooGi1020 <jungwk1020@gmail.com>
2024-06-29 18:22:48 +0900

    ✨ Feat: 프로필 정보 수정을 위한 로직 작성

--- src/components/Profiles/ProfileInfos.tsx
+++ src/components/Profiles/ProfileInfos.tsx
@@ -1,24 +1,45 @@
-import React, { memo } from 'react';
-import { Input, Label } from '../common/Form';
+import React, { ChangeEvent, memo } from 'react';
 
 const ProfileInfos = memo(
-  ({ label, value, isEditing }: { label: string; value: string; isEditing: boolean }) => {
+  ({
+    label,
+    value,
+    id,
+    isEditing,
+    isMyPage,
+    onChange,
+  }: {
+    label: string;
+    value: string;
+    id: string;
+    isEditing: boolean;
+    isMyPage: boolean;
+    onChange: (name: string, value: string | File | null) => void;
+  }) => {
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+      const { value } = e.target;
+      const { id } = e.target;
+      onChange(id, value);
+    };
+
     return (
       <div
-        className={`flex h-7 w-full items-center gap-[10px] ${isEditing ? 'md:mx-auto xl:mb-1' : ''}`}
+        className={`flex h-7 w-full items-center ${isEditing && isMyPage ? 'justify-center' : ''} gap-[10px] ${isEditing && isMyPage ? 'md:mx-auto xl:mb-1' : ''}`}
       >
-        {isEditing ? (
+        {isEditing && isMyPage ? (
           <>
-            <Label
-              htmlFor="profileInput"
+            <label
+              htmlFor={id}
               className="min-w-14 flex-none text-xs-regular text-grayscale-400 md:text-md-regular xl:min-w-16"
-              label={label}
-            />
-            <Input
+            >
+              {label}
+            </label>
+            <input
               name="profileInput"
-              id="profileInput"
+              id={id}
               className="flex-none rounded-10 bg-grayscale-100 py-2 pl-3 text-xs-regular text-grayscale-400 outline-none focus:border-2 focus:border-primary-green-200 sm:w-[70%] md:ml-2 md:w-[70%] md:text-md-regular lg:ml-0 lg:w-3/4 xl:w-[68%] xl:min-w-16"
               placeholder={value}
+              onChange={handleChange}
             />
           </>
         ) : (
