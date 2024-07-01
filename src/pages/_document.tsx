eb542ff8ef62bb477cdff4c460fbb5cec116dcbe
easyhyun00 <leejihyun0324@gmail.com>
2024-07-01 19:37:47 +0900

    ✨ Feat: styledComponents 오류해결

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-const Document = () => {
+const MyDocument = () => {
   return (
     <Html lang="ko">
       <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no" />
@@ -13,4 +21,36 @@ const Document = () => {
   );
 };
 
-export default Document;
+MyDocument.getInitialProps = async (
+  ctx: DocumentContext,
+): Promise<DocumentInitialProps & { styles: JSX.Element }> => {
+  const sheet = new ServerStyleSheet();
+  const originalRenderPage = ctx.renderPage;
+
+  try {
+    ctx.renderPage = () => {
+      return originalRenderPage({
+        enhanceApp: (App) => {
+          return (props) => {
+            return sheet.collectStyles(<App {...props} />);
+          };
+        },
+      });
+    };
+
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </>
+      ),
+    };
+  } finally {
+    sheet.seal();
+  }
+};
+
+export default MyDocument;
