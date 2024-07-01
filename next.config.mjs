d5bab868bd8905c70b0869c8f647a53071745b4e
easyhyun00 <leejihyun0324@gmail.com>
2024-07-01 20:00:53 +0900

    📦️ Chore: 코드 원상복귀

--- next.config.mjs
+++ next.config.mjs
@@ -14,9 +14,6 @@ const nextConfig = {
     });
     return config;
   },
-  compiler: {
-    styledComponents: true,
-  },
 };
 
 export default removeImports({})(nextConfig);
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,48 +1,16 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-    try {
-      ctx.renderPage = () => {
-        return originalRenderPage({
-          enhanceApp: (App) => {
-            return (props) => {
-              return sheet.collectStyles(<App {...props} />);
-            };
-          },
-        });
-      };
+const Document = () => {
+  return (
+    <Html lang="ko">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no" />
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
 
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
-    }
-  }
-
-  render() {
-    return (
-      <Html lang="ko">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no" />
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
-
-export default MyDocument;
+export default Document;
