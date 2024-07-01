aac4a56d3ab5bee1649a9357c167a2c1163d2bf0
dudwns0213 <dudwns0213@gmail.com>
2024-07-01 09:38:58 +0900

    ✨ Feat: boardid 페이지 구현 및 통합 export

---
+++ src/components/BoardDetail/index.ts
@@ -0,0 +1,5 @@
+export { default as ArticleCard } from './ArticleCard';
+export { default as CommentCount } from './CommentCount';
+export { default as CommentCard } from './CommentCard';
+export { default as CommentEdit } from './CommentEdit';
+export { default as CommentForm } from './CommentForm';
--- src/pages/board/[boardId].tsx
+++ src/pages/board/[boardId].tsx
@@ -1,10 +1,39 @@
+import { useState } from 'react';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
+import ArticleCard from '@/components/BoardDetail/ArticleCard';
+import { CommentFormData } from '@/types/apiType';
+import { postComment } from '@/lib/apis/comment/commentApi.api';
+import { CommentForm, CommentCount } from '@/components/BoardDetail';
 
 const BoardDetail = () => {
   const router = useRouter();
   const { boardId } = router.query;
+  const [isCommentSubmitted, setIsCommentSubmitted] = useState(false);
 
-  return <div>{boardId}번 게시물</div>;
+  const handleCommentSubmit = async (formData: CommentFormData) => {
+    try {
+      await postComment(Number(boardId), formData);
+      setIsCommentSubmitted(true); // 댓글 작성 완료 상태 변경
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <>
+      <ArticleCard id={boardId} />
+      <CommentForm onSubmit={handleCommentSubmit} />
+      <div>
+        <Link href="/boards">목록으로 돌아가기</Link>
+      </div>
+      <CommentCount
+        id={boardId}
+        isCommentSubmitted={isCommentSubmitted}
+        setIsCommentSubmitted={setIsCommentSubmitted}
+      />
+    </>
+  );
 };
 
 export default BoardDetail;
