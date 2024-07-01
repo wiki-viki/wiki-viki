ad4635df26bc5d1448246391bbb5dac40733bd93
easyhyun00 <leejihyun0324@gmail.com>
2024-07-01 19:44:34 +0900

    ✨ Feat: styledComponents 찐막

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,56 +1,48 @@
-import Document, {
-  DocumentContext,
-  DocumentInitialProps,
-  Html,
-  Head,
-  Main,
-  NextScript,
-} from 'next/document';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
-const MyDocument = () => {
-  return (
-    <Html lang="ko">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no" />
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-};
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () => {
+        return originalRenderPage({
+          enhanceApp: (App) => {
+            return (props) => {
+              return sheet.collectStyles(<App {...props} />);
+            };
+          },
+        });
+      };
 
-MyDocument.getInitialProps = async (
-  ctx: DocumentContext,
-): Promise<DocumentInitialProps & { styles: JSX.Element }> => {
-  const sheet = new ServerStyleSheet();
-  const originalRenderPage = ctx.renderPage;
-
-  try {
-    ctx.renderPage = () => {
-      return originalRenderPage({
-        enhanceApp: (App) => {
-          return (props) => {
-            return sheet.collectStyles(<App {...props} />);
-          };
-        },
-      });
-    };
-
-    const initialProps = await Document.getInitialProps(ctx);
-    return {
-      ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-          {sheet.getStyleElement()}
-        </>
-      ),
-    };
-  } finally {
-    sheet.seal();
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
-};
+
+  render() {
+    return (
+      <Html lang="ko">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no" />
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
 
 export default MyDocument;
