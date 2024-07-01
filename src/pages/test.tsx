65aa9905d44a82a1a132eb476461245eef00d52d
이지현 <leejihyun0324@gmail.com>
2024-07-01 20:35:57 +0900

    ✨ Feat: zustand persist 적용 (#73)
    ✨ Feat: zustand persis 적용

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -2,6 +2,8 @@ import dynamic from 'next/dynamic';
 import Modal from '@/components/common/Modal';
 import useBoolean from '@/hooks/useBoolean';
 import 'react-quill/dist/quill.snow.css';
+import { useAuthStore } from '@/store/userAuthStore';
+import { useStore } from '@/store/useStore';
 
 const EditorComponent = dynamic(
   () => {
@@ -23,9 +25,12 @@ const text1 =
 
 const Test = () => {
   const { value, handleOn, handleOff } = useBoolean();
-
+  const user = useStore(useAuthStore, (state) => {
+    return state.user;
+  });
   return (
     <div>
+      <p>{user?.name}</p>
       <EditorComponent value={text} theme="bubble" readOnly={true} modules={{ toolbar: null }} />
       <div className="my-6 border" />
       <EditorComponent value={text1} theme="snow" readOnly={true} modules={{ toolbar: false }} />
