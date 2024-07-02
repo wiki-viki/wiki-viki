13fa9e15805e8097ab8066965d0ae5c541e95625
dudwns0213 <dudwns0213@gmail.com>
2024-07-03 00:18:39 +0900

    💄 Design: 게시글 댓글 내용 컴포넌트 수정 및 디자인

--- src/components/BoardDetail/CommentCard.tsx
+++ src/components/BoardDetail/CommentCard.tsx
@@ -1,40 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 import { CommentResponse } from '@/types/apiType';
 import dateToString from '@/utils/dateToString';
 import { deleteComment } from '@/lib/apis/comment/commentApi.api';
-import { getMyInfo } from '@/lib/apis/user/userApi.api';
 import BasicProfileImage from '@/../public/images/basic_profile.png';
-import CommonButton from '../common/CommonButton';
+import DeleteIcon from '@/../public/svg/delete.svg';
+import EditIcon from '@/../public/svg/edit.svg';
+import ToastSelect from '@/components/common/ToastSelect';
 import CommentEditModal from './CommentEdit';
 
 interface CommentItemProps {
+  id: number;
   comment: CommentResponse;
   onDeleteComment: (commentId: number) => void;
   onCommentUpdated: (updatedComment: CommentResponse) => void;
 }
 
-const CommentCard = ({ comment, onDeleteComment, onCommentUpdated }: CommentItemProps) => {
+const CommentCard = ({ id, comment, onDeleteComment, onCommentUpdated }: CommentItemProps) => {
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
-  const [currentUserId, setCurrentUserId] = useState<number>();
-
-  useEffect(() => {
-    const fetchUserInfo = async () => {
-      try {
-        const userInfo = await getMyInfo();
-        setCurrentUserId(userInfo.id);
-      } catch (error) {
-        console.error('Error fetching user info:', error);
-      }
-    };
-    fetchUserInfo();
-  }, []);
 
   const handleDeleteComment = async () => {
     try {
       await deleteComment(comment.id);
       onDeleteComment(comment.id);
+      ToastSelect({ type: 'check', message: '댓글이 삭제되었습니다.' });
     } catch (error) {
       console.error('Delete error:', error);
     }
@@ -53,32 +44,49 @@ const CommentCard = ({ comment, onDeleteComment, onCommentUpdated }: CommentItem
 
   return (
     <>
-      <div className={`border-b border-grayscale-100 py-4 ${isEditing ? 'hidden' : ''}`}>
-        <div className="flex items-center justify-between">
-          <div className="relative size-[85px] rounded-full">
-            <Image
-              src={comment.writer.image || BasicProfileImage}
-              alt="프로필 이미지"
-              layout="fill"
-            />
+      <div className={`mt-7 ${isEditing ? 'hidden' : ''}`}>
+        <div className=" relative min-w-[320px] gap-2.5 rounded-10 px-5 py-4 shadow-md">
+          <div className="flex ">
+            <div className="relative size-10 shrink-0 rounded-full md:size-[50px]">
+              <Image
+                src={comment.writer.image || BasicProfileImage}
+                alt="프로필 이미지"
+                layout="fill"
+              />
+            </div>
+            <div className="  ml-[15px] flex-col break-words text-grayscale-500 lg:max-w-none">
+              <div className="text-lg-semibold lg:text-2lg-semibold ">{comment.writer.name}</div>
+              <div className="flex-wrap break-words text-md-regular lg:text-lg-regular">
+                {comment.content}
+              </div>
+              <div className="text-md-regular text-grayscale-400 lg:text-lg-regular">
+                {dateToString(comment.createdAt)}
+              </div>
+            </div>
           </div>
-          <div>{comment.writer.name}</div>
-          <div className="flex gap-2">
-            {currentUserId === comment.writer.id && (
+          <div className=" absolute right-5 top-4 flex justify-end gap-2">
+            {id === comment.writer.id && (
               <>
-                <CommonButton variant="secondary" onClick={handleEditComment}>
-                  수정
-                </CommonButton>
-                <CommonButton variant="secondary" onClick={handleDeleteComment}>
-                  삭제
-                </CommonButton>
+                <motion.div
+                  whileHover={{ scale: 1.2 }}
+                  transition={{ duration: 0.2 }}
+                  onClick={handleEditComment}
+                >
+                  <EditIcon />
+                </motion.div>
+                <motion.div
+                  whileHover={{ scale: 1.2 }}
+                  transition={{ duration: 0.2 }}
+                  onClick={handleDeleteComment}
+                >
+                  <DeleteIcon />
+                </motion.div>
               </>
             )}
           </div>
         </div>
-        <div>{comment.content}</div>
-        <div>{dateToString(comment.createdAt)}</div>
       </div>
+
       {isEditModalOpen && (
         <CommentEditModal comment={comment} onCommentUpdated={handleCommentUpdated} />
       )}
