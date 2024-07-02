a2a0e0792d4e62dbd1556d299288878a791c1ef8
easyhyun00 <leejihyun0324@gmail.com>
2024-07-02 23:19:03 +0900

    💄 Design: 자유게시판 테이블 수정

--- src/components/FreeBoards/BoardCard.tsx
+++ src/components/FreeBoards/BoardCard.tsx
@@ -26,23 +26,23 @@ const BoardCard = ({ board }: BoardCardProps) => {
           className="absolute rounded-t-10 object-cover"
         />
       </div>
-      <Link href={`/board/${board.id}`} rel="preload">
-        <div className="mx-[19px] mt-[18px] flex flex-col gap-[6px]">
-          <h3 className="truncate text-lg-semibold text-grayscale-500 md:text-2lg-semibold">
+      <div className="mx-[19px] mt-[18px] flex flex-col gap-[6px]">
+        <Link href={`/board/${board.id}`} rel="preload">
+          <h3 className="truncate text-lg-semibold text-grayscale-500 hover:text-primary-green-200 hover:underline md:text-2lg-semibold">
             {board.title}
           </h3>
-          <div className="flex flex-wrap justify-between text-xs-regular text-grayscale-400 md:text-sm-medium">
-            <div className="flex gap-1">
-              <span className="max-w-[70px] truncate md:max-w-[60px]">{board.writer.name}</span>
-              <span>{dateToString(board.createdAt).substring(2)}</span>
-            </div>
-            <span>
-              <span className="mr-[2px] text-primary-green-200">❤︎</span>
-              {board.likeCount}
-            </span>
+        </Link>
+        <div className="flex flex-wrap justify-between text-xs-regular text-grayscale-400 md:text-sm-medium">
+          <div className="flex gap-1">
+            <span className="max-w-[70px] truncate md:max-w-[60px]">{board.writer.name}</span>
+            <span>{dateToString(board.createdAt).substring(2)}</span>
           </div>
+          <span>
+            <span className="mr-[2px] text-primary-green-200">❤︎</span>
+            {board.likeCount}
+          </span>
         </div>
-      </Link>
+      </div>
     </motion.article>
   );
 };
