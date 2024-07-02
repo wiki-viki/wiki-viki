7451e8219786ffecb784d98886cddab87f8ef1f7
WooGi1020 <jungwk1020@gmail.com>
2024-07-02 21:13:16 +0900

    ✨ Feat: 회원 코드 없을 때 내 위키 클릭 시 처리 로직 생성

--- src/components/common/TopNavigationBar/AuthUserMenu.tsx
+++ src/components/common/TopNavigationBar/AuthUserMenu.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import { useAuthStore } from '@/store/userAuthStore';
+import { StyledToastContainer } from '@/styles/ToastStyle';
+import ToastSelect from '../ToastSelect';
 import MenuItem from './MenuItem';
 import MenuContainer from './MenuContainer';
 
@@ -13,7 +15,14 @@ interface UserMenuProps {
 const AuthUserMenu = ({ isOpen, handleClose, isMobile }: UserMenuProps) => {
   const { user, logout } = useAuthStore();
   const router = useRouter();
-  const code = user?.profile?.code;
+  const profile = user?.profile;
+
+  const handleCustomClose = () => {
+    if (!profile) {
+      ToastSelect({ type: 'error', message: '위키를 먼저 생성해주세요.' });
+    }
+    handleClose();
+  };
 
   const handleClickLogout = () => {
     logout();
@@ -22,38 +31,41 @@ const AuthUserMenu = ({ isOpen, handleClose, isMobile }: UserMenuProps) => {
   };
 
   return (
-    <MenuContainer isOpen={isOpen}>
-      {isMobile && (
-        <>
-          <MenuItem
-            onClick={handleClose}
-            title="위키목록"
-            href="/wikilist"
-            className="text-grayscale-600"
-          />
-          <MenuItem
-            onClick={handleClose}
-            title="자유게시판"
-            href="/boards"
-            className="text-grayscale-600"
-          />
-          <div className="mb-2 border-b" />
-        </>
-      )}
-      <MenuItem
-        onClick={handleClose}
-        title="계정 설정"
-        href="/mypage"
-        className="text-grayscale-600"
-      />
-      <MenuItem
-        onClick={handleClose}
-        title="내 위키"
-        href={`/wiki/${code}`}
-        className="text-grayscale-600"
-      />
-      <MenuItem onClick={handleClickLogout} title="로그아웃" className="text-grayscale-400" />
-    </MenuContainer>
+    <>
+      <StyledToastContainer limit={1} />
+      <MenuContainer isOpen={isOpen}>
+        {isMobile && (
+          <>
+            <MenuItem
+              onClick={handleClose}
+              title="위키목록"
+              href="/wikilist"
+              className="text-grayscale-600"
+            />
+            <MenuItem
+              onClick={handleClose}
+              title="자유게시판"
+              href="/boards"
+              className="text-grayscale-600"
+            />
+            <div className="mb-2 border-b" />
+          </>
+        )}
+        <MenuItem
+          onClick={handleClose}
+          title="계정 설정"
+          href="/mypage"
+          className="text-grayscale-600"
+        />
+        <MenuItem
+          onClick={handleCustomClose}
+          title="내 위키"
+          href={profile ? `/wiki/${profile?.code}` : `/`}
+          className="text-grayscale-600"
+        />
+        <MenuItem onClick={handleClickLogout} title="로그아웃" className="text-grayscale-400" />
+      </MenuContainer>
+    </>
   );
 };
 
