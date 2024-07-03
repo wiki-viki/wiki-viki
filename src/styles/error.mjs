1fe8b9888779a994191bb74d38d40f48e430ccfd
dudwns0213 <dudwns0213@gmail.com>
2024-07-03 10:55:49 +0900

    ✨ Feat: error 디자인 mjs로 변경

---
+++ src/styles/error.mjs
@@ -0,0 +1,65 @@
+const MAX_CLOVERS = 50;
+const CLOVER_CREATION_INTERVAL = 300;
+
+let clovers = [];
+let cloverCounter = 0;
+let lastTime = 0;
+let animationFrameId;
+
+export const createClover = () => {
+  if (clovers.length < MAX_CLOVERS) {
+    const clover = {
+      id: cloverCounter++,
+      top: Math.floor(Math.random() * -100),
+      left: Math.floor(Math.random() * window.innerWidth),
+      opacity: 1,
+      size: Math.random() * 10 + 10,
+    };
+    clovers.push(clover);
+    updateClovers();
+  }
+};
+
+export const updateClovers = () => {
+  const cloversContainer = document.getElementById('clovers-container');
+  cloversContainer.innerHTML = '';
+
+  clovers.forEach((clover) => {
+    const cloverElement = document.createElement('img');
+    cloverElement.src = '/image/wikiviki-clover.png';
+    cloverElement.alt = 'wikiviki';
+    cloverElement.width = clover.size;
+    cloverElement.height = clover.size;
+    cloverElement.style.position = 'absolute';
+    cloverElement.style.top = `${clover.top}px`;
+    cloverElement.style.left = `${clover.left}px`;
+    cloverElement.style.opacity = clover.opacity;
+    cloversContainer.appendChild(cloverElement);
+
+    clover.top += 1 + Math.random();
+  });
+
+  clovers = clovers.filter((clover) => {
+    return clover.top < window.innerHeight;
+  });
+};
+
+export const animate = (time) => {
+  if (time - lastTime > CLOVER_CREATION_INTERVAL) {
+    createClover();
+    lastTime = time;
+  }
+
+  updateClovers();
+  animationFrameId = requestAnimationFrame(animate);
+};
+
+export const startAnimation = () => {
+  animationFrameId = requestAnimationFrame(animate);
+};
+
+export const stopAnimation = () => {
+  cancelAnimationFrame(animationFrameId);
+};
+
+startAnimation();
