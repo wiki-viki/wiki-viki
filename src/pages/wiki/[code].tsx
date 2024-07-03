a95b8df75132e6d77de1a8f86e51aba7daf1b75e
WooGi1020 <jungwk1020@gmail.com>
2024-07-04 02:12:53 +0900

    ✨ Feat: ping 요청 로직 작성 및 적용

--- src/pages/wiki/[code].tsx
+++ src/pages/wiki/[code].tsx
@@ -12,8 +12,13 @@ import 'react-toastify/dist/ReactToastify.css';
 import { Editor, EditorMarkdown } from '@/components/Profiles/Editor';
 import { EDITOR_TEXT } from '@/constants/editorBasicText';
 import BasicWikiSection from '@/components/Profiles/BasicWikiSection';
-import { DetailProfileResponse, CodeType, ChangeProfilesFormData } from '@/types/apiType';
-import { getDetailProfiles, updateProfile } from '@/lib/apis/profile/profileApi.api';
+import {
+  DetailProfileResponse,
+  CodeType,
+  ChangeProfilesFormData,
+  PingFormData,
+} from '@/types/apiType';
+import { createPing, getDetailProfiles, updateProfile } from '@/lib/apis/profile/profileApi.api';
 import getImageUrl, { ImageData } from '@/lib/apis/image/imageApi.api';
 import { FORM_DATA_INIT } from '@/constants/formDataInitialValue';
 import Loading from '@/components/Loading';
@@ -21,6 +26,7 @@ import ToastSelect from '@/components/common/ToastSelect';
 import useIsMobile from '@/hooks/useIsMobile';
 import { useAuthStore } from '@/store/userAuthStore';
 import { useStore } from '@/store/useStore';
+import throttle from '@/utils/throttle';
 
 const noContentClassName = `text-lg-regular text-grayscale-400`;
 
@@ -44,6 +50,8 @@ const UserWikiPage: React.FC = () => {
 
   const [md, setMD] = useState<string | undefined>(undefined);
   const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const [answer, setAnswer] = useState<string | null>(null);
+  const [renewalTime, setRenewalTime] = useState<boolean>(false);
 
   const contentClassName = `
   w-full xl:absolute
@@ -80,6 +88,23 @@ const UserWikiPage: React.FC = () => {
     }
   };
 
+  const updateEditTime = async () => {
+    setRenewalTime((prev) => {
+      return !prev;
+    });
+    if (answer) {
+      const pingFormData = new FormData();
+      if (answer) {
+        pingFormData.append('securityAnswer', answer);
+      }
+      await createPing(code, pingFormData as unknown as PingFormData);
+    }
+  };
+
+  const setAnswerValue = (value: string) => {
+    setAnswer(value);
+  };
+
   const setEditorInitialValue = useCallback((value: string | null) => {
     setMD(value ? value : EDITOR_TEXT);
   }, []);
@@ -88,9 +113,12 @@ const UserWikiPage: React.FC = () => {
     handleOn();
   };
 
+  const throttlePing = throttle(updateEditTime, 5 * 6 * 9000);
+
   const handleEditorChange = useCallback(
     (value: string | undefined) => {
       setMD(value);
+      throttlePing();
       handleChange('content', value);
       if (!value) {
         handleChange('content', null);
@@ -129,6 +157,7 @@ const UserWikiPage: React.FC = () => {
         userProfile?.code,
         updatedFormData as ChangeProfilesFormData,
       );
+
       setUserProfile(profileUpdateResponse);
       setIsEditing(false);
     } catch (error: unknown) {
@@ -183,7 +212,7 @@ const UserWikiPage: React.FC = () => {
   }, [code]);
 
   useEffect(() => {
-    if (isEditing) {
+    if (isEditing || renewalTime) {
       if (timeoutRef.current) {
         clearTimeout(timeoutRef.current);
       }
@@ -204,7 +233,7 @@ const UserWikiPage: React.FC = () => {
         }
       };
     }
-  }, [isEditing]);
+  }, [renewalTime]);
 
   if (!userProfile) {
     return <Loading />;
@@ -257,7 +286,7 @@ const UserWikiPage: React.FC = () => {
 
       {isEditing && (
         <div className="ml-auto flex gap-3 sm:absolute sm:right-[60px] sm:top-[75px] md:absolute md:right-[90px] md:top-[75px] lg:top-[95px] xl:static xl:mt-[30px]">
-          <CommonButton variant="secondary" onClick={handleCancelClick}>
+          <CommonButton variant="secondary" onClick={handleCancelClick} className="bg-white">
             취소
           </CommonButton>
           <CommonButton variant="primary" onClick={handleSaveClick}>
@@ -272,6 +301,7 @@ const UserWikiPage: React.FC = () => {
           onClose={handleOff}
           setEditingMode={setEditingMode}
           code={userProfile.code}
+          setAnswer={setAnswerValue}
         />
       </Modal>
     </div>
