18ad0d0158e4435722e51d159e025ad3bdc6dad0
easyhyun00 <leejihyun0324@gmail.com>
2024-07-03 10:55:40 +0900

    🔨 Refactor: 카드 전체 클릭시 링크 이동 및 카드 hover 시 title 디자인 변경

--- src/components/FreeBoards/BoardCard.tsx
+++ src/components/FreeBoards/BoardCard.tsx
@@ -13,36 +13,37 @@ interface BoardCardProps {
 const BoardCard = ({ board }: BoardCardProps) => {
   return (
     <motion.article
-      className="relative h-[220px] w-full rounded-10 shadow-lg"
+      className="group relative h-[220px] w-full rounded-10 shadow-lg"
       whileHover={{ scale: 1.05 }}
       whileTap={{ scale: 0.95 }}
     >
-      <div className="relative h-3/5 w-full">
-        <Image
-          alt="게시물 이미지"
-          src={board.image ? board.image : EmptyImage}
-          fill
-          priority
-          className="absolute rounded-t-10 object-cover"
-        />
-      </div>
-      <div className="mx-[19px] mt-[18px] flex flex-col gap-[6px]">
-        <Link href={`/board/${board.id}`} rel="preload">
-          <h3 className="truncate text-lg-semibold text-grayscale-500 hover:text-primary-green-200 hover:underline md:text-2lg-semibold">
+      <Link href={`/board/${board.id}`} rel="preload">
+        <div className="relative h-3/5 w-full">
+          <Image
+            alt="게시물 이미지"
+            src={board.image ? board.image : EmptyImage}
+            fill
+            priority
+            className="absolute rounded-t-10 object-cover"
+          />
+        </div>
+        <div className="mx-[19px] mt-[18px] flex flex-col gap-[6px]">
+          <h3 className="group-hover truncate text-lg-semibold text-grayscale-500 md:text-2lg-semibold">
             {board.title}
           </h3>
-        </Link>
-        <div className="flex flex-wrap justify-between text-xs-regular text-grayscale-400 md:text-sm-medium">
-          <div className="flex gap-1">
-            <span className="max-w-[70px] truncate md:max-w-[60px]">{board.writer.name}</span>
-            <span>{dateToString(board.createdAt).substring(2)}</span>
+
+          <div className="flex flex-wrap justify-between text-xs-regular text-grayscale-400 md:text-sm-medium">
+            <div className="flex gap-1">
+              <span className="max-w-[70px] truncate md:max-w-[60px]">{board.writer.name}</span>
+              <span>{dateToString(board.createdAt).substring(2)}</span>
+            </div>
+            <span>
+              <span className="mr-[2px] text-primary-green-200">❤︎</span>
+              {board.likeCount}
+            </span>
           </div>
-          <span>
-            <span className="mr-[2px] text-primary-green-200">❤︎</span>
-            {board.likeCount}
-          </span>
         </div>
-      </div>
+      </Link>
     </motion.article>
   );
 };
