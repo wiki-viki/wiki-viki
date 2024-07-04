e4b6877747463c0ccc383547b2f0d245f6e255cb
Lin <shl6534@naver.com>
2024-07-04 20:15:48 +0900

    ✨ Feat: 유저 프로필 정보 전역 상태에 저장

--- src/store/userAuthStore.ts
+++ src/store/userAuthStore.ts
@@ -1,11 +1,13 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 import { getCookie, deleteCookie } from '@/utils/cookieUtil';
-import { UserResponse } from '@/types/apiType';
+import { UserResponse, DetailProfileResponse } from '@/types/apiType';
 
 interface AuthProps {
   user: UserResponse | null; // 유저 정보
+  userProfile: DetailProfileResponse | null; // 유저 프로필 정보
   saveUser: (user: UserResponse) => void; // 유저 정보 저장
+  saveUserProfile: (userProfile: DetailProfileResponse) => void; // 유저 프로필 정보 저장
   isLogin: boolean; // 로그인 여부
   checkLogin: () => void; // 로그인 여부 확인 함수
   logout: () => void; // 로그아웃
@@ -16,9 +18,13 @@ export const useAuthStore = create(
     (set) => {
       return {
         user: null,
+        userProfile: null,
         isLogin: false,
         saveUser: (user) => {
           set({ user, isLogin: true });
+        },
+        saveUserProfile: (userProfile) => {
+          set({ userProfile, isLogin: true });
         },
         checkLogin: () => {
           const accessToken = getCookie('accessToken');
@@ -31,6 +37,7 @@ export const useAuthStore = create(
         logout: () => {
           deleteCookie('accessToken');
           set({ user: null, isLogin: false });
+          set({ userProfile: null, isLogin: false });
         },
       };
     },
