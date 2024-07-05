3fd4d09cf2ff318487af0bfba5da757486f5dcc4
easyhyun00 <leejihyun0324@gmail.com>
2024-07-05 15:35:01 +0900

    ✨ Feat: 자유게시판 상세 메타태그 동적 적용

--- src/pages/board/[boardId]/index.tsx
+++ src/pages/board/[boardId]/index.tsx
@@ -5,7 +5,6 @@ import { ArticleComments } from '@/components/BoardDetail';
 import CommonButton from '@/components/common/CommonButton';
 import { useAuthStore } from '@/store/userAuthStore';
 import { useStore } from '@/store/useStore';
-import MetaTag from '@/components/common/MetaTag';
 
 const BoardDetail = () => {
   const router = useRouter();
@@ -20,21 +19,17 @@ const BoardDetail = () => {
   });
 
   return (
-    <>
-      <MetaTag title="상세 게시물" description="자유게시판 상세 게시물" url={`board/${boardId}`} />
-      <main className="mx-auto mt-[30px] max-w-[1060px] flex-col ">
-        <ArticleCard id={boardId} userId={userId} isLogin={isLogin} />
-
-        <div className="centerOfScreen mt-20 min-w-[320px] flex-col">
-          <Link href="/boards" rel="preload">
-            <CommonButton variant="secondary" className="my-8 px-10">
-              목록으로
-            </CommonButton>
-          </Link>
-        </div>
-        <ArticleComments id={boardId} userId={userId} isLogin={isLogin} />
-      </main>
-    </>
+    <main className="mx-auto mt-[30px] max-w-[1060px] flex-col ">
+      <ArticleCard id={boardId} userId={userId} isLogin={isLogin} />
+      <div className="centerOfScreen mt-20 min-w-[320px] flex-col">
+        <Link href="/boards" rel="preload">
+          <CommonButton variant="secondary" className="my-8 px-10">
+            목록으로
+          </CommonButton>
+        </Link>
+      </div>
+      <ArticleComments id={boardId} userId={userId} isLogin={isLogin} />
+    </main>
   );
 };
 
