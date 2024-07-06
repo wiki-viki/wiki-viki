9c7a7df32740e093bdfb38a744974acf5f74aaa5
Lin <shl6534@naver.com>
2024-07-06 10:40:03 +0900

    ✅ Test: Splash Screen 제거

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import { AnimatePresence, motion } from 'framer-motion'; // motion import 추가
 import Wrapper from '@/components/common/Container';
@@ -8,28 +8,28 @@ import '@uiw/react-md-editor/markdown-editor.css';
 import '@uiw/react-markdown-preview/markdown.css';
 import 'react-quill/dist/quill.snow.css';
 import 'react-toastify/dist/ReactToastify.css';
-import SplashScreen from '@/components/Landing/SplashScreen';
+// import SplashScreen from '@/components/Landing/SplashScreen';
 import '@/styles/globals.css';
 
 const App = ({ Component, pageProps, router }: AppProps) => {
-  const [showSplash, setShowSplash] = useState(true);
+  // const [showSplash, setShowSplash] = useState(true);
   const noNavBarPages = ['/', '/404', '/500'];
   const isNoNavBarPage = noNavBarPages.includes(router.pathname);
-  const isLandingPage = router.pathname === '/';
+  // const isLandingPage = router.pathname === '/';
 
-  useEffect(() => {
-    if (router.pathname !== '/') {
-      setShowSplash(false);
-    }
-  }, [router.pathname]);
+  // useEffect(() => {
+  //   if (router.pathname !== '/') {
+  //     setShowSplash(false);
+  //   }
+  // }, [router.pathname]);
 
-  const handleSplashFinish = () => {
-    setShowSplash(false);
-  };
+  // const handleSplashFinish = () => {
+  //   setShowSplash(false);
+  // };
 
-  if (showSplash && isLandingPage) {
-    return <SplashScreen onFinish={handleSplashFinish} />;
-  }
+  // if (showSplash && isLandingPage) {
+  //   return <SplashScreen onFinish={handleSplashFinish} />;
+  // }
 
   if (isNoNavBarPage) {
     return <Component {...pageProps} />;
